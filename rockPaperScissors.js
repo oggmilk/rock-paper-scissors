96e99ac08856604132c6c01b9eb63b6a28fdb639
oggmilk <mattdragicevic@gmail.com>
2022-09-26 13:39:18 -0700

    Adds buttons linked to single round player select

--- rockPaperScissors.js
+++ rockPaperScissors.js
@@ -1,46 +1,73 @@
 function getComputerChoice(){
-    let randomNum = Math.floor(Math.random()*3); //Generate random number between 0 and 2
-    if(randomNum == 0){ //assign game values based off these numbers
+    let randomNumber = Math.floor(Math.random()*3); //Generate random number between 0 and 2
+    if(randomNumber == 0){
         return "r";
-    }else if(randomNum == 1){
+    }else if(randomNumber == 1){
         return "p";
-    }else if(randomNum == 2){
+    }else if(randomNumber == 2){
         return "s";
     }
 }
 
-function singleRound(playerSelection, computerSelection){
+function singleRound(playerSelection){
+    computerSelection = "r"; ////makes comp selection rock right now for testing,, CHANGE BACK TO GET COMPCHOICE cuntion
     if(computerSelection == "r" && playerSelection == "s" || computerSelection == "p" && playerSelection == "r" || computerSelection == "s" && playerSelection == "p"){
-        return "Lose";
+        console.log("lose")
     }else if(computerSelection == "s" && playerSelection == "r" || computerSelection == "r" && playerSelection == "p" || computerSelection == "p" && playerSelection == "s"){
-        return "Win";
+        console.log("win")
     }else if(computerSelection == playerSelection){
-        return "Tie";
+        console.log("tie")
     }
-} //Establish win, lose, & tie conditions
-
-function game(){
-    let playerScore = 0;   
-    let computerScore = 0;
-    for (let i =0; i < 5; i++){ 
-        let playerEntry = prompt("Rock, Paper, or Scissors?") //prompt and add variable for user entry
-        let playerSelection = playerEntry.toLowerCase().slice(0,1); //reformat user entry for code compatibility
-        let computerSelection = getComputerChoice();  
-        if(computerSelection == "r" && playerSelection == "s" || computerSelection == "p" && playerSelection == "r" || computerSelection == "s" && playerSelection == "p"){
-            computerScore++;
-        }else if(computerSelection == "s" && playerSelection == "r" || computerSelection == "r" && playerSelection == "p" || computerSelection == "p" && playerSelection == "s"){
-            playerScore++;
-        } //adds points to scores after outcome
-        console.log(singleRound(playerSelection, computerSelection)); //displays round outcome
-        console.log("Your score:  ", playerScore, "  Computer score:  ", computerScore); //displays score
-    }
-    if(playerScore>computerScore){
-        console.log("You won the game!");
-    }else if (playerScore<computerScore){
-        console.log("You lost the game :(");
-    }else{
-        console.log("Tie Game: You are equally as smart as a computer nice!!!!")
-    }    // declares winner or loser or tie for the 5 rounds
 }
 
-game(); //initializes game
+
+
+const rock = document.querySelector("#rock");
+rock.addEventListener("click", function(){
+    singleRound("r")
+});
+
+const paper = document.querySelector("#paper");
+paper.addEventListener("click", function(){
+     singleRound("p")
+});
+
+const scissors = document.querySelector("#scissors");
+scissors.addEventListener("click", function(){
+    singleRound("s")
+});
+
+// let computerSelection = getComputerChoice();  
+// if(computerSelection == "r" && playerSelection == "s" || computerSelection == "p" && playerSelection == "r" || computerSelection == "s" && playerSelection == "p"){
+//     computerScore++;
+// }else if(computerSelection == "s" && playerSelection == "r" || computerSelection == "r" && playerSelection == "p" || computerSelection == "p" && playerSelection == "s"){
+//     playerScore++;
+// } //adds points to scores after outcome
+// console.log(singleRound(playerSelection, computerSelection)); //displays round outcome
+// console.log("Your score:  ", playerScore, "  Computer score:  ", computerScore); //displays score
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
