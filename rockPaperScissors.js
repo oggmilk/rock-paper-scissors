885a37e7c19a49929f86bf61d85d6a90d69885f0
oggmilk <mattdragicevic@gmail.com>
2022-09-26 15:39:48 -0700

    Adds style to display

--- rockPaperScissors.js
+++ rockPaperScissors.js
@@ -15,29 +15,25 @@ let computerScore = 0;
 function game(playerSelection){
     computerSelection = getComputerChoice();
     if(playerScore == 5 || computerScore == 5){
-        
-        return;
-        
+        return; 
     }
     if(computerSelection == "r" && playerSelection == "s" || computerSelection == "p" && playerSelection == "r" || computerSelection == "s" && playerSelection == "p"){
-        resultText.textContent = "You lose :(";
+        resultText.textContent = "You lost the round :(";
         computerScore++;
         console.log(score);
     }else if(computerSelection == "s" && playerSelection == "r" || computerSelection == "r" && playerSelection == "p" || computerSelection == "p" && playerSelection == "s"){
-        resultText.textContent = "You win :)";
+        resultText.textContent = "You won the round :)";
         playerScore++;
     }else if(computerSelection == playerSelection){
-        resultText.textContent = "You tie :|";
+        resultText.textContent = "You tied this round :|";
     } 
     playerScoreBoard.textContent = playerScore;
     computerScoreBoard.textContent = computerScore;
     if(playerScore == 5){
-        resultText.textContent = "You won!!!"
+        resultText.textContent = "You won and are smarter than a computer!!! Good job :)"
     }else if(computerScore == 5){
-        resultText.textContent = "You lost!!!!"
+        resultText.textContent = "You lost and are not as smart as a computer!!!! Bad job :("
     }
-    
-    
 }
 
 const playerScoreBoard = document.querySelector("#playerScore");
